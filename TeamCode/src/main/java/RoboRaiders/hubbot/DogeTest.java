d5f2c89ef50c9be5cfcce6164236a62fe72b3efb
anna <khakhalina.anna@gmail.com>
2019-10-19 16:08:51 -0400

    Doge Test Program

--- TeamCode/src/main/java/RoboRaiders/hubbot/DogeTest.java
+++ TeamCode/src/main/java/RoboRaiders/hubbot/DogeTest.java
@@ -1,79 +1,63 @@
 package RoboRaiders.hubbot;
 
-import com.disnodeteam.dogecv.CameraViewDisplay;
-import com.disnodeteam.dogecv.DogeCV;
-import com.disnodeteam.dogecv.detectors.skystone.GoldAlignDetector;
 import com.disnodeteam.dogecv.detectors.skystone.SkystoneDetector;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.opencv.core.Rect;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+
+
+
+import RoboRaiders.Logger.Logger;
+import easyopencv.examples.InternalCameraExample;
+import easyopencv.examples.WebcamExample;
 
 
 @TeleOp(name="Gold Example", group="DogeCV")
 
-public class DogeTest extends OpMode
+public class DogeTest extends LinearOpMode
 {
     // Detector object
     private SkystoneDetector detector;
+    private OpenCvInternalCamera phoneCam;
+
+    public void runOpMode()
+    {
+        Logger L = new Logger(String.valueOf("TEST"));
+        /**
+         * NOTE: Many comments have been omitted from this sample for the
+         * sake of conciseness. If you're just starting out with EasyOpenCv,
+         * you should take a look at {@link InternalCameraExample} or its
+         * webcam counterpart, {@link WebcamExample} first.
+         */
 
 
-    @Override
-    public void init() {
-        telemetry.addData("Status", "DogeCV 2019.1 - Gold Example");
-        // Set up detector
-        detector = new SkystoneDetector(); // Create detector
-        detector.init(hardwareMap.appContext, CameraViewDisplay.getInstance()); // Initialize it with the app context and camera
-        detector.useDefaults(); // Set detector to use default settings
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        phoneCam = new OpenCvInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
+        phoneCam.openCameraDevice();
+        detector = new SkystoneDetector();
+        phoneCam.setPipeline(detector);
+        phoneCam.startStreaming(640, 480, OpenCvCameraRotation.UPRIGHT);
 
-        // Optional tuning
-        detector.downscale = 0.4; // How much to downscale the input frames
+        waitForStart();
 
-        detector.areaScoringMethod = DogeCV.AreaScoringMethod.MAX_AREA; // Can also be PERFECT_AREA
-        //detector.perfectAreaScorer.perfectArea = 10000; // if using PERFECT_AREA scoring
-        detector.maxAreaScorer.weight = 0.005; //
-
-        detector.ratioScorer.weight = 5; //
-        detector.ratioScorer.perfectRatio = 1.0; // Ratio adjustment
-
-        detector.enable(); // Start the detector!
-
-
+        while (opModeIsActive())
+        {
+            telemetry.addData("Num contours found", detector.getScreenPosition());
+            telemetry.addData("rectangle", detector.foundRectangle());
+            telemetry.update();
+            sleep(100);
+        }
     }
 
-    /*
-     * Code to run REPEATEDLY when the driver hits INIT
-     */
-    @Override
-    public void init_loop() {
-    }
 
-    /*
-     * Code to run ONCE when the driver hits PLAY
-     */
-    @Override
-    public void start() {
 
-    }
 
-    /*
-     * Code to run REPEATEDLY when the driver hits PLAY
-     */
-    @Override
-    public void loop() {
-        telemetry.addData("IsFound: ", detector.isFound());
-        Rect rect = detector.getFoundRect();
-        if(detector.isFound()) telemetry.addData("Location: ", Integer.toString((int) (rect.x + rect.width*0.5)) + ", " + Integer.toString((int) (rect.y+0.5*rect.height)));
-        telemetry.update();
-    }
 
-    /*
-     * Code to run ONCE after the driver hits STOP
-     */
-    @Override
-    public void stop() {
-        // Disable the detector
-        if(detector != null) detector.disable();
-    }
+
+
+
+
 
 }
