6b35e5a51af912d3000b6a9b0de68e1b1f9d7b19
Roboraiders164 <roboraiders164@gmail.com>
2019-11-03 15:47:18 -0500

    Created Imuturn

--- TeamCode/src/main/java/RoboRaiders/AutonomousMethods/JarJarAutonomousMethods.java
+++ TeamCode/src/main/java/RoboRaiders/AutonomousMethods/JarJarAutonomousMethods.java
@@ -113,89 +113,39 @@ public abstract class JarJarAutonomousMethods extends LinearOpMode {
         }
     }
 
-    public void imuTurnPID(RoboRaidersPID rrPID, JarJarBot robot, float degreesToTurn, String direction) { //gets hardware from
-        double power = 0.0;
-        int loopcount = 0;
-
-
-        robot.motorFrontLeft.setDirection(DcMotor.Direction.FORWARD);
-        robot.motorBackLeft.setDirection(DcMotor.Direction.FORWARD);
-        rrPID.initialize();
-        telemetry.addLine().addData("in", "imuTurnPID");
-
-        currentHeading = 0.0;
+    public void imuTurn(JarJarBot robot, float degreesToTurn, double power, String direction) { //gets hardware from
+        //Robot and defines degrees as a
+        //float, power as a double, and direction as a string
+        //robot.angles = robot.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        //telemetry.addLine().addData("degreesToTurn",String.valueOf(degreesToTurn));
         currentHeading = robot.getIntegratedZAxis();
+        //finalHeading = currentHeading + degreesToTurn;
+        //telemetry.update();
 
+        // robot.getHeading(); returns the current heading of the IMU
 
         if (direction.equals("right")) { //if the desired direction is right
             finalHeading = currentHeading - degreesToTurn;
-            telemetry.addLine().addData("currentHeading", currentHeading);
-            telemetry.addLine().addData("finalHeading", finalHeading); // power = rrPID.CalculatePIDPowers(finalHeading,currentHeading);
-            telemetry.addLine().addData("power", power);
-
-            while ((opModeIsActive() && (loopcount < 20 &&
-                    !(currentHeading < finalHeading + 3.5 && currentHeading > finalHeading - 3.5)))) {
-                //&& Math.abs(power) > 0.1) {
-                currentHeading = robot.getIntegratedZAxis();
-                power = rrPID.CalculatePIDPowers(finalHeading, currentHeading) * 0.75;
-
-                loopcount++;
-
-                robot.setDriveMotorPower(power, power, power, power);
-
-                telemetry.addLine().addData("right", "right");
-                telemetry.addLine().addData("power", String.valueOf(power));
-                telemetry.addLine().addData("IntZ", String.valueOf(robot.getIntegratedZAxis()));
-                telemetry.addLine().addData("finalHeading", String.valueOf(finalHeading));
-                telemetry.addLine().addData("difference", String.valueOf(finalHeading - robot.getIntegratedZAxis()));
-                telemetry.update();
-
-            }
-            robot.setDriveMotorPower(0.0, 0.0, 0.0, 0.0); //stops robot
-        } else { //if the desired direction is left
-            finalHeading = currentHeading + degreesToTurn;
-
-            L.Debug("Turning Left");
-            L.Debug("finalHeading: ", finalHeading);
-
-
-            while ((opModeIsActive() && (loopcount < 20 &&
-                    !(currentHeading > finalHeading - 3.5 && currentHeading < finalHeading + 3.5)))) {
-                //&& Math.abs(power) > 0.1) {
-                currentHeading = robot.getIntegratedZAxis();
-                power = rrPID.CalculatePIDPowers(finalHeading, currentHeading) * 0.75;
-
-                loopcount++;
-
-                L.Debug("In While Loop");
-                L.Debug("finalHeading: ", finalHeading);
-                L.Debug("currentHeading: ", currentHeading);
-                L.Debug("Remaining Degrees: ", finalHeading - currentHeading);
-                L.Debug("Calculated PID Power (power): ", power);
-                L.Debug("loopcount", loopcount);
-
-
-                robot.setDriveMotorPower(power, power, power, power);
-
-
+            robot.setDriveMotorPower(power, -power, power, -power); //the robot will turn right
+            while(opModeIsActive() && robot.getIntegratedZAxis() > finalHeading) {
                 //robot.angles = robot.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
                 //currentHeading = robot.getIntegratedZAxis();
-                telemetry.addLine().addData("left", "left");
                 //telemetry.addLine().addData("getHeading",String.valueOf(currentHeading));
-                telemetry.addLine().addData("IntZ", String.valueOf(robot.integratedZAxis));
-                telemetry.addLine().addData("finalHeading", String.valueOf(finalHeading));
-                telemetry.addLine().addData("difference", String.valueOf(finalHeading - robot.getIntegratedZAxis()));
-                telemetry.update();
+                //telemetry.addLine().addData("IntZ",String.valueOf(robot.integratedZAxis));
+                //telemetry.update();
+
             }
         }
-
-
-        robot.setDriveMotorPower(0.0, 0.0, 0.0, 0.0); //stops robot
-
-        robot.motorFrontLeft.setDirection(DcMotor.Direction.REVERSE);
-        robot.motorBackLeft.setDirection(DcMotor.Direction.REVERSE);
-
-
-        L.Debug("End");
+        else { //if the desired direction is left
+            finalHeading = currentHeading + degreesToTurn;
+            robot.setDriveMotorPower(-power, power, -power, power); //the robot will turn left
+            while(opModeIsActive() && robot.getIntegratedZAxis() < finalHeading) {
+                //robot.angles = robot.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+                //currentHeading = robot.getIntegratedZAxis();
+                //telemetry.addLine().addData("getHeading",String.valueOf(currentHeading));
+                //telemetry.addLine().addData("IntZ",String.valueOf(robot.integratedZAxis));
+                //telemetry.update();
+            }
+        }
     }
 }
