7515504821a68cdbd87d17fa10a7c50f0bf31a1f
anna <khakhalina.anna@gmail.com>
2019-11-03 15:37:06 -0500

    imu pid

--- TeamCode/src/main/java/RoboRaiders/AutonomousMethods/JarJarAutonomousMethods.java
+++ TeamCode/src/main/java/RoboRaiders/AutonomousMethods/JarJarAutonomousMethods.java
@@ -3,9 +3,22 @@ package RoboRaiders.AutonomousMethods;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
 import RoboRaiders.Robot.JarJarBot;
 
 public abstract class JarJarAutonomousMethods extends LinearOpMode {
+
+    public double motor_power;
+    public double degreesToTurn;
+    public double currentHeading;
+    public double finalHeading;
+    public double integratedZAxis;
+    public double iza_lastHeading = 0.0;
+    public double iza_deltaHeading;
+    public float iza_newHeading;
+    public Orientation iza_angles;
+
 
     public void encodersMove(JarJarBot robot, double distance, double power, String direction) { //sets the parameters
 
@@ -42,7 +55,7 @@ public abstract class JarJarAutonomousMethods extends LinearOpMode {
         }
     }
 
-    public void encodersMoveStrafe(JarJarBot robot, double distance, double power, String direction){
+    public void encodersMoveStrafe(JarJarBot robot, double distance, double power, String direction) {
         robot.resetEncoders();
         robot.runWithEncoders();
 
@@ -80,7 +93,7 @@ public abstract class JarJarAutonomousMethods extends LinearOpMode {
         robot.runWithoutEncoders(); //sets the mode back to run without encoder
     }
 
-    public void runIntake(JarJarBot robot, double power){
+    public void runIntake(JarJarBot robot, double power) {
         double startIntakeTime = System.currentTimeMillis();
         robot.intakeMotorLeft.setPower(power);
         robot.intakeMotorRight.setPower(power);
@@ -99,15 +112,14 @@ public abstract class JarJarAutonomousMethods extends LinearOpMode {
             Thread.currentThread().interrupt();
         }
     }
-    public void imuTurnPID(RoboRaidersPID rrPID, NostromoBotMotorDumper robot, float degreesToTurn, String direction) { //gets hardware from
+
+    public void imuTurnPID(RoboRaidersPID rrPID, JarJarBot robot, float degreesToTurn, String direction) { //gets hardware from
         double power = 0.0;
         int loopcount = 0;
 
 
         robot.motorFrontLeft.setDirection(DcMotor.Direction.FORWARD);
         robot.motorBackLeft.setDirection(DcMotor.Direction.FORWARD);
-
-        LoggerOId L = new LoggerOId("imuTurnPID");
         rrPID.initialize();
         telemetry.addLine().addData("in", "imuTurnPID");
 
@@ -115,84 +127,51 @@ public abstract class JarJarAutonomousMethods extends LinearOpMode {
         currentHeading = robot.getIntegratedZAxis();
 
 
-        L.Debug("Start");
-        L.Debug("currentHeading: ", currentHeading);
-        L.Debug("degreesToTurn", degreesToTurn);
-
-        // robot.getHeading(); returns the current heading of the IMU
-
         if (direction.equals("right")) { //if the desired direction is right
             finalHeading = currentHeading - degreesToTurn;
             telemetry.addLine().addData("currentHeading", currentHeading);
-            telemetry.addLine().addData("finalHeading", finalHeading);
-
-            L.Debug("Turning Right");
-            L.Debug("finalHeading: ",finalHeading);
-
-            // power = rrPID.CalculatePIDPowers(finalHeading,currentHeading);
+            telemetry.addLine().addData("finalHeading", finalHeading); // power = rrPID.CalculatePIDPowers(finalHeading,currentHeading);
             telemetry.addLine().addData("power", power);
 
-            L.Debug("Calculated PID Power (power): ",power);
-
-            //  robot.setDriveMotorPower(power, power, power, power); //the robot will turn right
-
-            while((opModeIsActive() && (loopcount < 20 &&
-                    !(currentHeading < finalHeading + 3.5 && currentHeading > finalHeading - 3.5)))){
+            while ((opModeIsActive() && (loopcount < 20 &&
+                    !(currentHeading < finalHeading + 3.5 && currentHeading > finalHeading - 3.5)))) {
                 //&& Math.abs(power) > 0.1) {
                 currentHeading = robot.getIntegratedZAxis();
-                power = rrPID.CalculatePIDPowers(finalHeading,currentHeading) * 0.75;
+                power = rrPID.CalculatePIDPowers(finalHeading, currentHeading) * 0.75;
 
                 loopcount++;
 
-                L.Debug("In While Loop");
-                L.Debug("finalHeading: ",finalHeading);
-                L.Debug("currentHeading: ",currentHeading);
-                L.Debug("Remaining Degrees: ",finalHeading - currentHeading);
-                L.Debug("Calculated PID Power (power): ",power);
-                L.Debug("loopcount", loopcount);
-
-
                 robot.setDriveMotorPower(power, power, power, power);
 
-                //robot.angles = robot.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
-                //currentHeading = robot.getIntegratedZAxis();
                 telemetry.addLine().addData("right", "right");
                 telemetry.addLine().addData("power", String.valueOf(power));
-                telemetry.addLine().addData("IntZ",String.valueOf(robot.getIntegratedZAxis()));
-                telemetry.addLine().addData("finalHeading",String.valueOf(finalHeading));
-                telemetry.addLine().addData("difference",String.valueOf(finalHeading - robot.getIntegratedZAxis()));
+                telemetry.addLine().addData("IntZ", String.valueOf(robot.getIntegratedZAxis()));
+                telemetry.addLine().addData("finalHeading", String.valueOf(finalHeading));
+                telemetry.addLine().addData("difference", String.valueOf(finalHeading - robot.getIntegratedZAxis()));
                 telemetry.update();
 
             }
             robot.setDriveMotorPower(0.0, 0.0, 0.0, 0.0); //stops robot
-            L.Debug("Out of While Loop");
-            L.Debug("finalHeading: ",finalHeading);
-            L.Debug("currentHeading: ",robot.getIntegratedZAxis());
-            L.Debug("Remaining Degrees: ",finalHeading - robot.getIntegratedZAxis());
-
-
-
-        }
-        else { //if the desired direction is left
+        } else { //if the desired direction is left
             finalHeading = currentHeading + degreesToTurn;
 
             L.Debug("Turning Left");
-            L.Debug("finalHeading: ",finalHeading);
+            L.Debug("finalHeading: ", finalHeading);
 
 
-            while((opModeIsActive() && (loopcount < 20 &&
-                    !(currentHeading > finalHeading - 3.5 && currentHeading < finalHeading + 3.5)))){
+            while ((opModeIsActive() && (loopcount < 20 &&
+                    !(currentHeading > finalHeading - 3.5 && currentHeading < finalHeading + 3.5)))) {
                 //&& Math.abs(power) > 0.1) {
                 currentHeading = robot.getIntegratedZAxis();
-                power = rrPID.CalculatePIDPowers(finalHeading,currentHeading) * 0.75;
+                power = rrPID.CalculatePIDPowers(finalHeading, currentHeading) * 0.75;
 
                 loopcount++;
 
                 L.Debug("In While Loop");
-                L.Debug("finalHeading: ",finalHeading);
-                L.Debug("currentHeading: ",currentHeading);
-                L.Debug("Remaining Degrees: ",finalHeading - currentHeading);
-                L.Debug("Calculated PID Power (power): ",power);
+                L.Debug("finalHeading: ", finalHeading);
+                L.Debug("currentHeading: ", currentHeading);
+                L.Debug("Remaining Degrees: ", finalHeading - currentHeading);
+                L.Debug("Calculated PID Power (power): ", power);
                 L.Debug("loopcount", loopcount);
 
 
@@ -203,9 +182,9 @@ public abstract class JarJarAutonomousMethods extends LinearOpMode {
                 //currentHeading = robot.getIntegratedZAxis();
                 telemetry.addLine().addData("left", "left");
                 //telemetry.addLine().addData("getHeading",String.valueOf(currentHeading));
-                telemetry.addLine().addData("IntZ",String.valueOf(robot.integratedZAxis));
-                telemetry.addLine().addData("finalHeading",String.valueOf(finalHeading));
-                telemetry.addLine().addData("difference",String.valueOf(finalHeading - robot.getIntegratedZAxis()));
+                telemetry.addLine().addData("IntZ", String.valueOf(robot.integratedZAxis));
+                telemetry.addLine().addData("finalHeading", String.valueOf(finalHeading));
+                telemetry.addLine().addData("difference", String.valueOf(finalHeading - robot.getIntegratedZAxis()));
                 telemetry.update();
             }
         }
@@ -218,4 +197,5 @@ public abstract class JarJarAutonomousMethods extends LinearOpMode {
 
 
         L.Debug("End");
+    }
 }
