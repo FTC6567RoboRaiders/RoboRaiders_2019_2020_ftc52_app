d8f3a346f31e3c30aeac5d8cf8e9ef98f79dd265
anna <khakhalina.anna@gmail.com>
2019-11-17 18:54:42 -0500

    brightness detection auto methods

--- TeamCode/src/main/java/RoboRaiders/AutonomousMethods/JarJarAutonomousMethods.java
+++ TeamCode/src/main/java/RoboRaiders/AutonomousMethods/JarJarAutonomousMethods.java
@@ -16,6 +16,7 @@ import org.openftc.easyopencv.OpenCvInternalCamera;
 import org.openftc.easyopencv.OpenCvPipeline;
 
 import RoboRaiders.JarJarsAutonomous.BrightnessDetectionAuto;
+import RoboRaiders.JarJarsAutonomous.RoboRaidersPipeline;
 import RoboRaiders.Robot.JarJarBot;
 import RoboRaiders.hubbot.BrightnessDetection;
 
@@ -176,22 +177,20 @@ public abstract class JarJarAutonomousMethods extends LinearOpMode {
         }
     }
 
-    public void stoneDetection(JarJarBot robot, int pattern){
+    public void stoneDetection(JarJarBot robot, RoboRaidersPipeline ){
             OpenCvCamera phone_camera;
-            RoboRaiders.JarJarsAutonomous.BrightnessDetectionAuto.SamplePipeline stone_pipeline;
-            public int pattern = 999;
+            RoboRaiders.JarJarsAutonomous.BrightnessDetectionAuto.RoboRaidersPipeline stone_pipeline;
+            int pattern = 999;
             public void runOpMode() throws InterruptedException{
                 int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
 
                 phone_camera = new OpenCvInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
                 phone_camera.openCameraDevice();
 
-                stone_pipeline = new RoboRaiders.JarJarsAutonomous.BrightnessDetectionAuto.SamplePipeline();
+                stone_pipeline = new RoboRaiders.JarJarsAutonomous.BrightnessDetectionAuto.RoboRaidersPipeline();
                 phone_camera.setPipeline(stone_pipeline);
 
                 phone_camera.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
-
-                waitForStart();
 
                 while (opModeIsActive() && pattern == 999) {
                     //super.updateTelemetry(telemetry);
@@ -212,126 +211,9 @@ public abstract class JarJarAutonomousMethods extends LinearOpMode {
                 telemetry.addData("PATTERN", pattern);
                 telemetry.update();
 
-                Thread.sleep(2000);
+                //Thread.sleep(2000);
             }
 
-            class SamplePipeline extends OpenCvPipeline {
-                int left_hue;
-                int right_hue;
-
-                int left_br;
-                int right_br;
-
-
-                public Mat processFrame(Mat input, int pattern) {
-
-                    input.convertTo(input, CV_8UC1, 1, 10);
-
-                    int[] left_rect = {
-                            //the second number here possibly represents the amount of sections you are splitting the screen into??   32
-                            (int) (input.cols() * (9f / 32f)), //the first number goes from left to right increasing, controls x axis
-                            (int) (input.rows() * (5f / 32f)), //the first number here controls the y axis
-                            (int) (input.cols() * (15f / 32f)),
-                            (int) (input.rows() * (7f / 32f))
-                    };
-
-                    int[] right_rect = {
-                            (int) (input.cols() * (17f / 32f)),
-                            (int) (input.rows() * (5f / 32f)),
-                            (int) (input.cols() * (23f / 32f)),
-                            (int) (input.rows() * (7f / 32f))
-                    };
-
-                    Imgproc.rectangle(
-                            input,
-                            new Point(
-                                    left_rect[0],
-                                    left_rect[1]),
-
-                            new Point(
-                                    left_rect[2],
-                                    left_rect[3]),
-                            new Scalar(0, 255, 0), 1);
-
-                    Imgproc.rectangle(
-                            input,
-                            new Point(
-                                    right_rect[0],
-                                    right_rect[1]),
-
-                            new Point(
-                                    right_rect[2],
-                                    right_rect[3]),
-                            new Scalar(0, 0, 255), 1);
-
-                    Mat left_block = input.submat(left_rect[1], left_rect[3], left_rect[0], left_rect[2]);
-                    Mat right_block = input.submat(right_rect[1], right_rect[3], right_rect[0], right_rect[2]);
-
-
-                    Scalar left_mean = Core.mean(left_block);
-
-
-                    Scalar right_mean = Core.mean(right_block);
-
-                    left_hue = get_hue((int) left_mean.val[0], (int) left_mean.val[1], (int) left_mean.val[2]);
-                    right_hue = get_hue((int) right_mean.val[0], (int) right_mean.val[1], (int) right_mean.val[2]);
-                    left_br = get_brightness((int) left_mean.val[0], (int) left_mean.val[1], (int) left_mean.val[2]);
-                    right_br = get_brightness((int) right_mean.val[0], (int) right_mean.val[1], (int) right_mean.val[2]);
-
-                    if (left_br > 100 && right_br > 100) pattern = 1;
-                        //skystone is not in frame
-                        //above 100 is normal, bellow 100 is skystone
-                    else if (left_br > 100 && right_br < 100) pattern = 2;
-                        //skystone is on right
-                    else if (left_br < 100 && right_br > 100) pattern = 3;
-                        //skystone is on left
-                    else if (left_br < 100 && right_br < 100) {
-                        if (left_br > right_br) {
-                            pattern = 1;
-                        } else if (left_br < right_br) {
-                            pattern = 2;
-                        } else {
-                            pattern = 3;
-                        }
-                    }
-                    telemetry.addData("position", pattern);
-                    telemetry.update();
-                    sleep(100);
-
-                    return input;
-                }
-            }
-
-
-            private int get_hue(int red, int green, int blue) {
-
-                float min = Math.min(Math.min(red, green), blue);
-                float max = Math.max(Math.max(red, green), blue);
-
-                if (min == max) {
-                    return 0;
-                }
-
-                float hue = 0f;
-                if (max == red) {
-                    hue = (green - blue) / (max - min);
-
-                } else if (max == green) {
-                    hue = 2f + (blue - red) / (max - min);
-
-                } else {
-                    hue = 4f + (red - green) / (max - min);
-                }
-
-                hue = hue * 60;
-                if (hue < 0) hue = hue + 360;
-
-                return Math.round(hue);
-            }
-
-            private int get_brightness(int red, int green, int blue) {
-                return (int) (((double) (red + green + blue)) / 3);
-            }
 
     }
 }
