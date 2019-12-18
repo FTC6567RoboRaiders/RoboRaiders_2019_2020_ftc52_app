e149dafa381e8032875cc0e94e96c7ee88d949d1
skocik <skocik@us.ibm.com>
2019-12-18 16:13:17 -0500

    Updates to encodertestteleop added comments

--- TeamCode/src/main/java/RoboRaiders/hubbot/EncoderTestTeleop.java
+++ TeamCode/src/main/java/RoboRaiders/hubbot/EncoderTestTeleop.java
@@ -6,30 +6,27 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name = "HubbotServoTest", group = "Hubbot")
-@Disabled
+@TeleOp(name = "Hubbot Encoder Teleop", group = "Hubbot")
 public class EncoderTestTeleop extends LinearOpMode {
 
   private DcMotor motor1;
   private DcMotor motor2;       // Motor objects
 
 
-  public boolean currStateY = false;
-  public boolean prevStateY = false;
-  public boolean currStateX = false;
-  public boolean prevStateX = false;
-  public double servoPos = 0.0;
+  public boolean currStateY = false;      // current state of the gamepad1 Y button
+  public boolean currStateX = false;      // current state of the gamepad1 X button
+  public boolean rightsticky = false;     // determines if right stick y should be used for input
+
 
   @Override
   public void runOpMode() {
 
-    // get a reference to our Light Sensor object.
+    // get a reference to motor1 from the hardware map, reset the encoders and set the zero power behaviour
     motor1 = hardwareMap.get(DcMotor.class, "motor1");
-
     motor1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     motor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-    motor1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
+    // get a reference to motor2 from the hardware map, reset encoders and run without encoder s
     motor2 = hardwareMap.get(DcMotor.class, "motor2");
     motor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     motor2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -43,36 +40,49 @@ public class EncoderTestTeleop extends LinearOpMode {
     // Note we use opModeIsActive() as our loop condition because it is an interruptible method.
     while (opModeIsActive()) {
 
+      // save the state of the gamepad 1 X and Y buttons
       currStateX = gamepad1.x;
       currStateY = gamepad1.y;
 
+      // motor 2 is only controlled by the left stick Y direction
       motor2.setPower(-gamepad1.left_stick_y);
 
       // send the info back to driver station using telemetry function.
       telemetry.addData("currStateX", currStateX);
       telemetry.addData("currStateY", currStateY);
 
+      // When the X button is pressed on gamepad 1, motor1 will run to encoder position 5000 at 50% power
       if (currStateX) {
-        if (!motor1.isBusy()) {
-
+          rightsticky = false;                        // indicate that gamepad1 right stick input is to be ignored
           motor1.setTargetPosition(5000);
+          motor1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
           motor1.setPower(0.5);
           telemetry.addData("motor1 RUN_TO_POSITION, 5000, power: 0.5, currentPosition: ", motor1.getCurrentPosition());
-        }
-//        else {
-//          motor1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-//          motor1.setPower(0.0);
-//          telemetry.addData("motor1 RUN_WITHOUT_ENCODER, power: ", "0.0");
-//        }
       }
+
+      // When the Y button is pressed on gamepad 1, motor1 will run to encoder position 0 at 50% power
       else if (currStateY) {
+        rightsticky = false;                          // indicate that gamepad1 right stick input is to be ignored
         motor1.setTargetPosition(0);
+        motor1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         motor1.setPower(-0.5);
         telemetry.addData("motor1 RUN_TO_POSITION, 0, power: -0.5, currentPosition: ", motor1.getCurrentPosition());
       }
 
 
+      // if the gamepad1 right stick has been pushed (that is it is not zero), then indicate that the gamepad1 right stick
+      // input is valid and should be applied to the power for motor1
+      if (gamepad1.right_stick_y != 0.0) {
+        motor1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightsticky = true;
+      }
 
+      // when gamepad1 right stick input is valid apply a power motor1
+      if (rightsticky) {
+        motor1.setPower(-gamepad1.right_stick_y);
+      }
+
+      telemetry.addData("motor1 RUN_TO_POSITION, currentPosition: ", motor1.getCurrentPosition());
       telemetry.update();
     }
   }
