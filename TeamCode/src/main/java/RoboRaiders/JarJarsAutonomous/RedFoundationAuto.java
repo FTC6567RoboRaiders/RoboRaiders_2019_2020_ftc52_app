f4f7c1d4ae5d5197f00d70d630985c103e76f68c
Roboraiders164 <roboraiders164@gmail.com>
2019-12-27 09:04:42 -0500

    initialize robot

--- TeamCode/src/main/java/RoboRaiders/JarJarsAutonomous/RedFoundationAuto.java
+++ TeamCode/src/main/java/RoboRaiders/JarJarsAutonomous/RedFoundationAuto.java
@@ -10,8 +10,20 @@ import RoboRaiders.Robot.RobotTelemetryDisplay;
 
 public class RedFoundationAuto extends RRAutonomousMethods {
 
+    Robot robot = new Robot();
+
     @Override
     public void runOpMode(){
 
+        robot.initialize(hardwareMap);
+        waitForStart();
+
+        encodersMoveRTP(robot, 30, .8, "backward"); //robot moves backwards 30 inches
+        robot.setFoundationGrabberGrabbed(); //foundation servos go down
+        encodersMoveRTP(robot, 15, .8, "forward"); //robot moves forwards 15 inches
+        imuTurn(robot, 90, .6, "right"); //robot turns 90 degrees right
+        encodersMoveRTP(robot, 20, .8, "backward"); //robot moves to wall
+        robot.setFoundationGrabberUnGrabbed(); //foundation servos come up
+        encodersMoveRTP(robot, 50, .8, "forward"); //robot parks under SkyBridge
     }
 }
